f2758efeca1be654ac19f1857bb605a7a0d7b375
Victor Matheus <victormatheus183@gmail.com>
2023-01-07 16:23:17 -0300

    GameObjects randomizer

---
+++ Assets/Scripts/Utils/GameObjectRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    public class GameObjectRandomizer : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _gameObjectsList;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void CreateRandomGameObject()
+        {
+            var choosedGameobject = _gameObjectsList.GetRandomItem();
+            Instantiate(choosedGameobject, transform);
+        }
+    }
+}
