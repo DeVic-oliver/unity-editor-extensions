5d1902155d4698510b5dd24e96531a057761d6af
Victor Matheus <victormatheus183@gmail.com>
2023-01-07 16:23:01 -0300

    Custom editor created
    A creator for random game objects in a list

---
+++ Assets/Scripts/Editor/RandomGameObjectsCreator.cs
@@ -0,0 +1,22 @@
+using Assets.Scripts.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(GameObjectRandomizer))]
+public class RandomGameObjectsCreator : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        GameObjectRandomizer gameObjectRandomizer = (GameObjectRandomizer)target;
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Create random Gameobject"))
+        {
+            gameObjectRandomizer.CreateRandomGameObject();
+        }
+    }
+}
