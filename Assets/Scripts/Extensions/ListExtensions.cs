11ce41b5e5fa46e6f3e9a930c6025a7675fc2866
Victor Matheus <victormatheus183@gmail.com>
2023-01-07 16:23:39 -0300

    extension to get a random item from a list

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+
+public static class ListExtensions 
+{
+    public static T GetRandomItem<T>(this List<T> list)
+    {
+        Random randomizer = new Random();
+        int listSize = list.Count;
+        if(listSize == 1)
+        {
+            return list[0];
+        }
+        int index = randomizer.Next(0, listSize);
+        return list[index];
+    }
+}
